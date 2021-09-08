b804013a86f87d97e70cc8acb9570a2e418792bc
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2021-09-08 19:12:57 -0300

    Versão 1.1 do desafio eleições da Blue

--- Desafio4.js
+++ Desafio4.js
@@ -1,43 +1,38 @@
 const prompt = require("prompt-sync")();
 
-// Receber votos até que o usuário diga que não tem mais ninguém para votar;
-
-// Ter uma função chamada autorizaVoto(anoNascimento) retornando: "Negado`, "Opcional"  ou "Obrigatório"; OK
+// Função para verificar se o eleitor tem idade permitida para votar.
 let nascimento = +prompt("Digite o ano do seu nascimento: ");
 console.log();
 function autorizaVoto(nascimento) {
   if (nascimento >= 2006) {
     return console.log(
-      "PREZADO ELEITOR: Votação negada, você não pode votar!\n"
+      "\33[1;31m PREZADO ELEITOR: Votação negada, você não pode votar!\n"
     );
-  } else if ((nascimento < 2006 && nascimento >= 2004) || nascimento < 1946) {
+  } else if ((nascimento < 2006 && nascimento >= 2004) || nascimento < 1951) {
     return console.log(
-      "PREADO ELEITOR: Votação opcional, você não é obrigado a votar!\n"
+      "\033[1;34m PREZADO ELEITOR: Votação opcional, você pode votar, mas, não é obrigado!\n"
     );
   } else {
-    return console.log("PREZADO ELEITOR: Você está obrigado a votar!\n");
+    return console.log(
+      "\033[1;33m PREZADO ELEITOR: Você está obrigado a votar!\n"
+    );
   }
 }
+autorizaVoto(nascimento);
 
-
-/*Ter uma função chamada votacao(autorizacao, voto) que valida  e contabiliza o voto (número entre 1 e 5) ou 
-retorna a mensagem: "Você não pode votar", caso o voto não possa ser contabilizado; */
-function votacao(autorizacao) {
+// Fução para autorizar a votação ou não.
+function votacao(nascimento) {
   if (nascimento >= 2006) {
-    return "Voce não está autorizado a votar!\n";
+    return console.log("PREZADO ELEITOR: Voce não está autorizado a votar!\n");
   } else {
     //    return console.log("Inicie a votação!")
-    return "Você está autorizado a votar, vamos iniciar a votação:\n ";
+    return console.log(
+      "\033[1;32m Você está autorizado a votar, vamos iniciar a votação:\n "
+    );
   }
 }
-
-
-/* Contabilizar os votos de acordo com os significados 
-1 = Candidato 1
-2 = Candidato 2
-3 = Candidato 3
-4 = Voto Nulo`
-5 = Voto em Branco */
+votacao(nascimento);
+// variáeis para contagem dos votos do
 let candidato1 = 0;
 let candidato2 = 0;
 let candidato3 = 0;
@@ -46,45 +41,75 @@ let candidato5 = 0;
 let votos;
 
 if (nascimento <= 2007) {
+  console.log("\033[1;33m Inicio de votação: \n");
   while (true) {
-    console.log("Inicio de votação: ");
     console.log(
-      "Escolha seu voto:\n 1 = Candidato nº 1\n 2 = Candidato nº 2\n 3 = Candidato nº 3\n 4 = Voto nulo\n 5 = Voto em branco\n 0 = Para encerrar a votação"
+      "\033[1;31m Escolha:\n\033[1;37m 1 = Candidato nº 1\n 2 = Candidato nº 2\n 3 = Candidato nº 3\n 4 = Voto nulo\n 5 = Voto em branco\n 0 = Para encerrar a votação\n"
     );
-    votos = +prompt("Digite o seu voto: ");
+    votos = +prompt("\033[1;32m Digite o seu voto: ");
     while (votos < 0 && votos > 5) {
-      votos = +prompt("Digite seu voto: ");
+      console.log("\033[1;33m Próximo voto: \n");
     }
-    switch (votos) {
-      case 1:
-        candidato1++;
+    if (votos < 0 || votos > 5) {
+      console.log("\n\033[1;36m Escolha uma opção válida!\n");
+    } else {
+      console.log("\033[1;33m Próximo voto: \n");
+      switch (votos) {
+        case 1:
+          candidato1++;
+          break;
+        case 2:
+          candidato2++;
+          break;
+        case 3:
+          candidato3++;
+          break;
+        case 4:
+          candidato4++;
+          break;
+        case 5:
+          candidato5++;
+          break;
+      }
+      if (votos == 0) {
         break;
-      case 2:
-        candidato2++;
-        break;
-      case 3:
-        candidato3++;
-        break;
-      case 4:
-        candidato4++;
-        break;
-      case 5:
-        candidato5++;
-        break;
-    }
-    if (votos == 0) {
-      break;
+      }
     }
   }
-} 
-    console.log(``candidato1);
-    console.log(candidato2);
-    console.log(candidato3);
-    console.log(candidato4);
-    console.log(candidato5);
+}
+
+console.log("\033[1;36m");
+// console.log(`O candidato 1 recebeu ${candidato1} voto(s)`);
+// console.log(`O candidato 2 recebeu ${candidato2} voto(s)`);
+// console.log(`O candidato 3 recebeu ${candidato3} voto(s)`);
+// console.log(`Votos nulos recebeu ${candidato4} voto(s)`);
+// console.log(`Votos em branco recebeu ${candidato5} voto(s)`);
 
 /* Ter uma função chamada exibirResultados() que deve mostrar:
  - O total de votos para cada candidato 
  - O total de votos nulos
  - O total de votos em branco
  - Qual candidato venceu a votação */
+listaVotados = [candidato1, candidato2, candidato3];
+listaVotados.push();
+//  console.log(listaVotados);
+
+function exibirResultados(listaVotados) {
+  console.log(`O candidato 1 recebeu ${candidato1} voto(s)`);
+  console.log(`O candidato 2 recebeu ${candidato2} voto(s)`);
+  console.log(`O candidato 3 recebeu ${candidato3} voto(s)`);
+  console.log(`Votos nulos recebeu ${candidato4} voto(s)`);
+  console.log(`Votos em branco recebeu ${candidato5} voto(s)`);
+  console.log();
+  if (candidato1 > candidato2 && candidato1 > candidato3) {
+    console.log("\033[1;33m")
+    return console.log(`O candidato 1 foi eleito com ${candidato1} voto(s) dos votos válidos`);
+  } else if (candidato2 > candidato1 && candidato2 > candidato3) {
+    return console.log(`O candidato 2 foi eleito com ${candidato2} voto(s) dos votos válidos`);
+  } else (candidato3 > candidato1 && candidato3 > candidato2)
+  return console.log(`O candidato 3 foi eleito com ${candidato3} voto(s) do total de votos válidos`);
+  
+}
+  
+exibirResultados(votos);
+
